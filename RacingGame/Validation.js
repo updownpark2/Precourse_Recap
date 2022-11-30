899515aa3f47ac0466e305d63e0753f729d10e36
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-30 12:38:11 +0900

    feat: 입력값 타당성 검사 (자동차 이름)

---
+++ RacingGame/Validation.js
@@ -0,0 +1,46 @@
+const { carName } = require("./UserInput");
+
+class Validation {
+  carName(carName) {
+    let carNameArr = carName.split(",");
+    this.checkEnglish(carNameArr);
+    this.checkDuplication(carNameArr);
+    this.checkComma(carNameArr);
+    this.checkLength(carNameArr);
+  }
+
+  checkEnglish(carNameArr) {
+    if (/^[a-zA-Z]*$/g.test(carNameArr) === false) {
+      throw new Error(`[ERROR] 영어,영어의 형태로 입력이 가능합니다.`);
+    }
+  }
+  checkDuplication(carNameArr) {
+    let checkArr = [];
+    let i = 0;
+    for (; i < carNameArr.length; i++) {
+      if (checkArr.includes(carNameArr[i])) {
+        throw new Error(`[ERROR] 중복된 이름은 사용할 수 없습니다.`);
+      }
+      checkArr.push(carNameArr[i]);
+    }
+  }
+  checkComma(carNameArr) {
+    carNameArr.forEach((carName) => {
+      if (carName === ``) {
+        throw new Error(`[ERROR] ,,가 연속으로 입력 되었습니다.`);
+      }
+    });
+  }
+
+  checkLength(carNameArr) {
+    carNameArr.forEach((carName) => {
+      if (carName.length > 5) {
+        throw new Error(
+          `[ERROR] ,를 써주지 않으셨거나 이름이 5글자 이상입니다.`
+        );
+      }
+    });
+  }
+}
+
+module.exports = Validation;
