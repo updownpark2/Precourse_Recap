7d49255b248c80ff6a35fb5e87560d385280a71b
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-30 12:27:14 +0900

    feat: 경주할 자동차 이름 입력받는 기능

---
+++ RacingGame/UserInput.js
@@ -0,0 +1,11 @@
+const { Console } = require("@woowacourse/mission-utils");
+const UserInput = {
+  carName: (callback) => {
+    Console.readLine(
+      `경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분`,
+      callback
+    );
+  },
+};
+
+module.exports = UserInput;
