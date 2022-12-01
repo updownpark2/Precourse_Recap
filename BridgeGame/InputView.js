9857fa3236d6b24b0b34d03c462829cf2a83dc09
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-01 15:21:46 +0900

    feat: 이동할 칸을 입력받는 기능

--- BridgeGame/InputView.js
+++ BridgeGame/InputView.js
@@ -4,6 +4,10 @@ const InputView = {
   bridgeLength: (callback) => {
     Console.readLine(`다리 길이를 입력해주세요.`, callback);
   },
+
+  userMove: (callback) => {
+    Console.readLine(`이동할 칸을 입력해주세요.`, callback);
+  },
 };
 
 module.exports = InputView;
