665abb3f4fa687f26a1a758c7cd5f1157132652c
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-01 15:02:09 +0900

    feat: 다리 길이에 대한 입력값 받기

---
+++ BridgeGame/InputView.js
@@ -0,0 +1,9 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const InputView = {
+  bridgeLength: (callback) => {
+    Console.readLine(`다리 길이를 입력해주세요.`, callback);
+  },
+};
+
+module.exports = InputView;
