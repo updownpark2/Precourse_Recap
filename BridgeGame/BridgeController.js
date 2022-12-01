1c33e99a33eaaaa7c33e8cd5334ad89e05775205
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-01 15:21:26 +0900

    refactor: 오류 발생  후 그 시점에서 입력을 다시 받을 수 있도록 함수 구조 변경

--- BridgeGame/BridgeController.js
+++ BridgeGame/BridgeController.js
@@ -1,15 +1,30 @@
 const OutputView = require("./OutputView");
 const InputView = require("./InputView");
 const Validation = require("./Validation");
-
+const { Console } = require("@woowacourse/mission-utils");
 const validation = new Validation();
 
 class BridgeController {
-  gameStart() {
+  gameStartMent() {
     OutputView.startMent();
+  }
+
+  gameStart() {
     InputView.bridgeLength((bridgeLength) => {
-      validation.bridgeLength(bridgeLength);
+      if (this.checkValidation(bridgeLength) === false) {
+        this.gameStart();
+      }
     });
+  }
+
+  checkValidation(bridgeLength) {
+    try {
+      validation.bridgeLength(bridgeLength);
+    } catch (error) {
+      Console.print(error);
+
+      return false;
+    }
   }
 }
 
