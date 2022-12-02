7da72b834e2ea69d942e0e57d3661fa11378164f
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-02 13:26:00 +0900

    feat: 결과를 출력하는 기능

--- BridgeGame/OutputView.js
+++ BridgeGame/OutputView.js
@@ -4,6 +4,11 @@ const OutputView = {
   startMent() {
     Console.print(`로또 게임을 시작합니다잉🐸🐸`);
   },
+
+  result(result) {
+    Console.print(`[ ${result[0].join(` | `).split(`, `)} ]`);
+    Console.print(`[ ${result[1].join(` | `).split(`, `)} ]`);
+  },
 };
 
 module.exports = OutputView;
