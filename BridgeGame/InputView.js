6bfdbeaaedfa629039b1c942f77c9c67d496bfaf
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-02 14:04:22 +0900

    feat: 재시도/종료 입력값을 받는 기능

--- BridgeGame/InputView.js
+++ BridgeGame/InputView.js
@@ -2,11 +2,17 @@ const { Console } = require("@woowacourse/mission-utils");
 
 const InputView = {
   bridgeLength: (callback) => {
-    Console.readLine(`다리 길이를 입력해주세요.`, callback);
+    Console.readLine(`다리 길이를 입력해주세요.\n`, callback);
   },
 
   userMove: (callback) => {
-    Console.readLine(`이동할 칸을 입력해주세요.`, callback);
+    Console.readLine(`이동할 칸을 입력해주세요.\n`, callback);
+  },
+  retry: (callback) => {
+    Console.readLine(
+      `게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n`,
+      callback
+    );
   },
 };
 
