5867bfd14aad6fecb7067449e32060acb3b7b57b
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-05 21:28:39 +0900

    feat(Controller): 함수를 호출하여 입력한 todor가 화면에 보이도록함

--- Todolist/Controller.js
+++ Todolist/Controller.js
@@ -1,22 +1,52 @@
 import { TodoMake } from "./TodoMake.js";
-import InputView from "./InputView.js";
+import { InputView } from "./InputView.js";
+import { OutputView } from "./OutputView.js";
+
+const todoMake = new TodoMake();
+const inputView = new InputView();
+const outputView = new OutputView();
 
 export class Controller {
-  #todoMake = new TodoMake();
-
   start($target) {
     this.makeFormAndInput($target);
-    this.getUserInput();
+    this.makeTodoBoxAndUl($target);
+    this.showAllTodo();
+    this.getUserInput($target);
+    this.onClickButton();
   }
 
   makeFormAndInput($target) {
-    this.#todoMake.makeForm($target);
+    todoMake.makeForm($target);
+  }
+  makeTodoBoxAndUl($target) {
+    todoMake.makeTodobox($target);
   }
 
   getUserInput() {
-    InputView((event) => {
+    inputView.setInput((event) => {
       event.preventDefault();
+      const userTodo = event.target[0].value;
+      event.target[0].value = ``;
+      this.saveTodo(userTodo);
+      this.showPlusTodo(userTodo);
+    });
+  }
+
+  onClickButton() {
+    const Button = document.querySelector(`li button`);
+    Button.addEventListener("click", (event) => {
       console.log(event.target);
     });
+  }
+  saveTodo(userTodo) {
+    todoMake.saveLocal(userTodo);
+  }
+
+  showAllTodo() {
+    outputView.userTodo();
+  }
+
+  showPlusTodo(userTodo) {
+    outputView.plusTodo(userTodo);
   }
 }
