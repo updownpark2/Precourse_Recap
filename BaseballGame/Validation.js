6aea26f39bbd1b82382e6d550905e1ddefa28066
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-06 22:50:22 +0900

    feat: 입력값 타당성 검사 (재시작/종료)

--- BaseballGame/Validation.js
+++ BaseballGame/Validation.js
@@ -1,6 +1,17 @@
 class Validation {
+  checkRange(userInput) {
+    let checkArr = [];
+    let i = 0;
+    for (; i < 3; i++) {
+      if (checkArr.includes(userInput[i])) {
+        return false;
+      }
+      checkArr.push(userInput[i]);
+    }
+    return true;
+  }
+
   CheckNumber(number) {
-    console.log(number.split(""));
     if (number.split("").includes(" ")) {
       throw new Error(`[ERROR] 공백이 있습니다.`);
     }
@@ -9,6 +20,18 @@ class Validation {
     }
     if (/^[1-9]*$/g.test(number) === false) {
       throw new Error(`[ERROR] 숫자만 입력이 가능합니다.`);
+    }
+    if (this.checkRange(number) === false) {
+      throw new Error(`[ERROR] 중복값이 있으면 안됩니다.`);
+    }
+  }
+
+  checkRetry(retry) {
+    if (/^[1-2]*$/g.test(retry) === false) {
+      throw new Error(`[ERROR] 숫자만 입력이 가능합니다.`);
+    }
+    if (retry.length !== 1) {
+      throw new Error(`[ERROR] 1 혹은 2 둘 중 한 숫자만 입력해주세요.`);
     }
   }
 }
