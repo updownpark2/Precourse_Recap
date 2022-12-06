1fdd74347df0ad6abd8537796e6129b8600a988e
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-06 18:45:13 +0900

    feat: userTodo를 localstorage에 저장할 수 있도록 함

--- Todolist/MakeTodo.js
+++ Todolist/MakeTodo.js
@@ -1,4 +1,6 @@
 export class MakeTodo {
+  #userTodoArr = [];
+
   makeFormAndInput($target) {
     const form = document.createElement(`form`);
     form.id = `FORM`;
@@ -11,4 +13,24 @@ export class MakeTodo {
     input.id = `INPUT`;
     input.placeholder = `Todo`;
   }
+
+  makeUl($target) {
+    const ul = document.createElement(`ul`);
+    ul.id = `UL`;
+    $target.appendChild(ul);
+  }
+
+  setUserTodoArr() {
+    if (localStorage.getItem(`todo`) === null) {
+      this.#userTodoArr = [];
+      return;
+    }
+    this.#userTodoArr = JSON.parse(localStorage.getItem(`todo`));
+  }
+
+  saveUserTodo(userTodo) {
+    this.setUserTodoArr();
+    this.#userTodoArr.push(userTodo);
+    localStorage.setItem(`todo`, JSON.stringify(this.#userTodoArr));
+  } //여기서 저장
 }
