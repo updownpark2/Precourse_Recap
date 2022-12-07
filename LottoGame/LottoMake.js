d99adeb1de81e16f1667578f728233b01957b54b
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-07 18:20:01 +0900

    fix: Loto가 만들어지지 않는 오류 수정

--- LottoGame/LottoMake.js
+++ LottoGame/LottoMake.js
@@ -2,7 +2,8 @@ const { Random } = require("@woowacourse/mission-utils");
 
 function LottoMake(lottoCount) {
   let Lotto = [];
-  for (; lottoCount < 6; i++) {
+  let i = 0;
+  for (; i < lottoCount; i++) {
     const numbers = Random.pickUniqueNumbersInRange(1, 45, 6);
     Lotto.push(numbers);
   }
