4a12a811c7888b09e58db3dee7025fc1cb1fa4d7
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-07 17:31:30 +0900

    feat: 구매한 로또 개수 판단

--- LottoGame/LottoController.js
+++ LottoGame/LottoController.js
@@ -1,13 +1,20 @@
 const InputView = require(`./InputView`);
 const Validation = require(`./Validation`);
+const LottoRule = require("./LottoRule");
 
 const validation = new Validation();
+const lottoRule = new LottoRule();
 
 class LottoController {
   getMoney() {
     InputView.money((money) => {
       validation.checkMoney(money);
+      const lottoCount = this.#getLottoCount(money);
     });
+  }
+
+  #getLottoCount(money) {
+    return lottoRule.getLottoCount(money);
   }
 }
 
---
+++ LottoGame/LottoRule.js
@@ -0,0 +1,7 @@
+class LottoRule {
+  getLottoCount(money) {
+    return money / 1000;
+  }
+}
+
+module.exports = LottoRule;
