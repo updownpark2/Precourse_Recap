671252736ba5b9362c695150624830247c222dcf
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-08 14:26:19 +0900

    feat: 랜덤 로또 생성 기능 추가

--- LottoGameRetry/Controller.js
+++ LottoGameRetry/Controller.js
@@ -1,16 +1,21 @@
 const InputView = require(`./InputView`);
 const Validation = require(`./Validation`);
 const OutputView = require(`./Outputview`);
+const LottoRule = require(`./LottoRule`);
+const makeLotto = require("./makeLotto");
 
 class Controller {
   constructor() {
     this.validation = new Validation();
+    this.lottoRule = new LottoRule();
   }
 
   getMoney() {
     InputView.getMoney((money) => {
       if (this.#checkMoney(money) !== false) {
-        this.#showLottoCount(money);
+        const lottoCount = this.#getLottoCount(money);
+        this.#showLottoCount(lottoCount);
+        const lotto = this.#getLotto(lottoCount);
       }
     });
   }
@@ -29,8 +34,18 @@ class Controller {
     OutputView.showError(error);
   }
 
-  #showLottoCount(money) {
-    OutputView.showLottoCount(money);
+  #showLottoCount(lottoCount) {
+    OutputView.showLottoCount(lottoCount);
+  }
+
+  #getLottoCount(money) {
+    const lottoCount = this.lottoRule.getLottoCount(money);
+
+    return lottoCount;
+  }
+
+  #getLotto(lottoCount) {
+    return makeLotto(lottoCount);
   }
 }
 
---
+++ LottoGameRetry/makeLotto.js
@@ -0,0 +1,15 @@
+const { Random } = require("@woowacourse/mission-utils");
+
+function makeLotto(lottoCount) {
+  let lottoArr = [];
+  let index = 0;
+  for (; index < lottoCount; index++) {
+    const numbers = Random.pickUniqueNumbersInRange(1, 45, 6).sort(
+      (a, b) => a - b
+    );
+    lottoArr.push(numbers);
+  }
+  return lottoArr;
+}
+
+module.exports = makeLotto;
