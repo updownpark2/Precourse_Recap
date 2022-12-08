01e29a51afbb149996855938a926b8ade28d3a54
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-12-08 16:30:12 +0900

    로또 게임 완성

--- LottoGameRetry/App.js
+++ LottoGameRetry/App.js
@@ -1,1 +1,10 @@
-const { Console } = require("@woowacourse/mission-utils");
+const Controller = require("./Controller");
+
+class App {
+  play() {
+    const controller = new Controller();
+    controller.getMoney();
+  }
+}
+
+module.exports = App;
